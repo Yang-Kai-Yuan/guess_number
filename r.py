ff74727963e576f65753ed45230553c2b3c6f227
yang-kai-yuan <haha200ha@gmail.com>
2020-10-04 21:59:55 +0800

    guess nuber game2

--- r.py
+++ r.py
@@ -1,6 +1,8 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+    count += 1
     guess = input('請輸入一個數字')
     guess = int(guess)
     if guess == r:
@@ -10,3 +12,4 @@ while True:
     	print('你猜得太大了，請再猜一次')
     else:
     	print('你猜得太小了，請再猜一次')
+    print('這是你猜的第', count,'次')
